d0b6ce1c766a3a245b6b6a04769980cd2c6dad01
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-27 08:10:53 -0300

    style: adiciona animação na landing

--- src/components/landing/main/Main.jsx
+++ src/components/landing/main/Main.jsx
@@ -10,12 +10,14 @@ export default function Main() {
     return (
         <main className={styles.main}>
             <section className={styles.heroSection}>
-                <div className={styles.heroDiv}>
-                    <h1>Organize-se e aumente sua produtividade!</h1>
-                    <p>Tenha total controle das suas tarefas</p>
-                    <div className={styles.imgDiv}>
-                        <img src={appStore} alt="App Store" />
-                        <img src={googlePlay} alt="Google Play" />
+                <div className={styles.heroContainer}>
+                    <div className={styles.heroDiv}>
+                        <h1>Organize-se e aumente sua produtividade!</h1>
+                        <p>Tenha total controle das suas tarefas</p>
+                        <div className={styles.imgDiv}>
+                            <img src={appStore} alt="App Store" />
+                            <img src={googlePlay} alt="Google Play" />
+                        </div>
                     </div>
                 </div>
             </section>
@@ -24,21 +26,21 @@ export default function Main() {
                 <div className={styles.infoDiv}>
                     <h2>Adicione #tags para maior controle</h2>
                     <p>Separe suas tarefas com identificadores para maior organização</p>
-                    <img src={tags} alt="Tags" className={styles.tagsImg} />
+                    <img src={tags} alt="Tags" className={`${styles.tagsImg} ${styles.animatedImg}`} />
                 </div>
             </section>
             <section className={styles.weekSection}>
                 <div className={styles.infoDiv}>
                     <h2>Se planeje! Saiba como estará o clima nos próximos dias</h2>
                     <p>Integração com API climática que exibe as condições climáticas dos próximos dias</p>
-                    <img src={week} alt="Week" className={styles.weekImg} />
+                    <img src={week} alt="Week" className={`${styles.weekImg} ${styles.animatedImg}`} />
                 </div>
             </section>
             <section className={styles.geminiSection}>
                 <div className={styles.infoDiv}>
                     <h2>Receba dicas úteis do Gemini</h2>
                     <p>Integração com inteligência artificial que fornece dicas para se organizar e cumprir suas tarefas dos jeito mais eficiente</p>
-                    <img src={gemini} alt="Gemini" className={styles.geminiImg} />
+                    <img src={gemini} alt="Gemini" className={`${styles.geminiImg} ${styles.animatedImg}`} />
                 </div>
             </section>
             <section className={styles.downloadSection}>
